ceaa069790b2e14ba0e9090e8c1c40e6e9f152c3
Ohlsson Noah <Noah.Ohlsson@vaxjo.se>
2024-11-16 00:02:09 +0100

    knapp fixad + små fix

--- visaStandardPlatser.js
+++ visaStandardPlatser.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded"), function(){
-    const toggleButton = document.getElementById('StandardKnapp');
-    const toggleElement = document.getElementById('standardPlatser');
+const toggleButton = document.getElementById('StandardKnapp');
+const toggleElement = document.getElementById('standardPlatser');
 
-    toggleButton.addEventListener('click'), function() {
+document.addEventListener("DOMContentLoaded", function(){
+    toggleButton.addEventListener('click', function() {
         if (toggleElement.style.visibility === 'hidden')
         {
             toggleElement.style.visibility = 'visible';
@@ -11,5 +11,5 @@ document.addEventListener("DOMContentLoaded"), function(){
         {
             toggleElement.style.visibility = 'hidden';
         }
-    }
-}
+    });
+});
