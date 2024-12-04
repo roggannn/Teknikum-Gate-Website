cacd02e1bd7ea672ae33095d783052a6bfb9e727
roggannn <144336997+roggannn@users.noreply.github.com>
2024-12-04 16:56:41 +0100

    biljett fixar

---
+++ standardräknare.js
@@ -0,0 +1,15 @@
+let value2 = 0;
+
+function plus2() {
+    if (value2 < 10) {
+        value2++;
+        document.getElementById("antalLabel2").textContent = value2;
+    }
+}
+
+function minus2() {
+    if (value2 > 0) {
+        value2--;
+        document.getElementById("antalLabel2").textContent = value2;
+    }
+}
---
+++ vipräknare.js
@@ -0,0 +1,15 @@
+let value = 0;
+
+function plus() {
+    if (value < 10) {
+        value++;
+        document.getElementById("antalLabel").textContent = value;
+    }
+}
+
+function minus() {
+    if (value > 0) {
+        value--;
+        document.getElementById("antalLabel").textContent = value;
+    }
+}
