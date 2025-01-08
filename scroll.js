3a40d067b4f5b5915f008983fe3f0153cfc911e8
roggannn <144336997+roggannn@users.noreply.github.com>
2025-01-08 23:21:48 +0100

    scroll script för biljetter

---
+++ scroll.js
@@ -0,0 +1,17 @@
+document.querySelector('.biljett1').addEventListener('click', function() {
+    const target = document.querySelector('.beställning');
+
+    target.scrollIntoView({
+        behavior:"smooth",
+        block: "start",
+    });
+});
+
+document.querySelector('.biljett2').addEventListener('click', function() {
+    const target = document.querySelector('.beställning');
+
+    target.scrollIntoView({
+        behavior:"smooth",
+        block: "start",
+    });
+});
