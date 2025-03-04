b44ef217d00f2e37acedf8d15d9f7f96857b5a49
roggannn <144336997+roggannn@users.noreply.github.com>
2025-03-04 11:45:11 +0100

    PRIS FÖRÄNDRING

--- prisberäkning.js
+++ prisberäkning.js
@@ -1,6 +1,6 @@
 let pris = 0;
 
 function prisförändring() {
-    pris = document.getElementById("antalLabel").textContent * 250 + document.getElementById("antalLabel2").textContent * 200 + " kr";
+    pris = document.getElementById("antalLabel").textContent * 250 + document.getElementById("antalLabel2").textContent * 150 + " kr";
     document.getElementById("priset").textContent = "Det kostar: " + pris;
 }
